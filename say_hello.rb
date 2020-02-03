5d99061bdc4629fc6b62075c8e1f337c6d7a3100
Edward Lindsey <eddielindsey23@gmail.com>
2020-02-03 06:29:21 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -4,6 +4,6 @@ end
 def say_hello
   puts "Hello Ruby Programmer!"
 end
-def say_hello(name="Gabriela")
-  puts "Hello, #{Gabriela}. We heard you are a great #{Ruby} programmer."
+def say_hello(name="Ruby Programmer")
+  puts "Hello, #{Ruby Programmer}
 end
